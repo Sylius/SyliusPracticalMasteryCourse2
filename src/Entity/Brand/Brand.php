422d288ce8539228ccb797803c28375cc5fb28da
Gracjan Józefczyk <juzkos@gmail.com>
2025-12-23 13:20:10 +0100

    How to create custom form

--- src/Entity/Brand/Brand.php
+++ src/Entity/Brand/Brand.php
@@ -8,13 +8,20 @@ use App\Entity\Product\Product;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 use Sylius\Resource\Model\CodeAwareInterface;
 use Sylius\Resource\Model\ResourceInterface;
+use Sylius\Resource\Model\TimestampableInterface;
+use Sylius\Resource\Model\TimestampableTrait;
+use Sylius\Resource\Model\ToggleableInterface;
+use Sylius\Resource\Model\ToggleableTrait;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_brand')]
-class Brand implements ResourceInterface, CodeAwareInterface
+class Brand implements ResourceInterface, CodeAwareInterface, ToggleableInterface, TimestampableInterface
 {
+    use ToggleableTrait, TimestampableTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
@@ -28,6 +35,20 @@ class Brand implements ResourceInterface, CodeAwareInterface
 
     #[ORM\OneToMany(targetEntity: Product::class, mappedBy: 'brand')]
     private Collection $products;
+
+    #[ORM\Column(type: 'boolean')]
+    /** @var bool */
+    protected $enabled = true;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    #[Gedmo\Timestampable(on: 'create')]
+    /** @var \DateTimeInterface|null */
+    protected $createdAt;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    #[Gedmo\Timestampable(on: 'update')]
+    /** @var \DateTimeInterface|null */
+    protected $updatedAt;
 
     public function __construct()
     {
---
+++ src/Form/Type/BrandType.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use Sylius\Bundle\ResourceBundle\Form\EventSubscriber\AddCodeFormSubscriber;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class BrandType extends AbstractResourceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->addEventSubscriber(new AddCodeFormSubscriber())
+            ->add('name', TextType::class, [
+                'label' => 'sylius.ui.name',
+                'required' => true,
+            ])
+            ->add('enabled', CheckboxType::class, [
+                'label' => 'sylius.ui.enabled',
+            ])
+        ;
+    }
+}
