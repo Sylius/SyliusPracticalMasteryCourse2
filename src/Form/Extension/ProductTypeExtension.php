118ccad014ed148c762d5b558146e22a5f4a99f4
Gracjan Józefczyk <juzkos@gmail.com>
2025-12-28 08:20:41 +0100

    How to customize existing form

---
+++ src/Form/Extension/ProductTypeExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Extension;
+
+use App\Entity\Brand\Brand;
+use Sylius\Bundle\AdminBundle\Form\Type\ProductType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ProductTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('brand', EntityType::class, [
+                'label' => 'sylius.ui.brand',
+                'class' => Brand::class,
+                'choice_label' => 'name',
+                'placeholder' => 'sylius.ui.choose_brand',
+            ])
+        ;
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [
+            ProductType::class,
+        ];
+    }
+}
