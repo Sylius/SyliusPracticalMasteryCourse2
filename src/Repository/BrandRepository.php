d76c2682523406d27e545e9ca3eafdc80cb03bb6
Gracjan Józefczyk <juzkos@gmail.com>
2025-12-30 07:54:29 +0100

    Resource controller in twig

--- src/Repository/BrandRepository.php
+++ src/Repository/BrandRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Entity\Brand\Brand;
 use Doctrine\ORM\QueryBuilder;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Sylius\Resource\Doctrine\Persistence\RepositoryInterface;
@@ -15,6 +16,24 @@ final class BrandRepository extends EntityRepository implements RepositoryInterf
         return $this->createQueryBuilder('b')
             ->andWhere('b.enabled = :enabled')
             ->setParameter('enabled', true)
+        ;
+    }
+
+    public function findEnabled(): array
+    {
+        return $this->createEnabledQueryBuilder()
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findOneByCode(string $code): ?Brand
+    {
+        return $this->createQueryBuilder('b')
+            ->andWhere('b.code = :code')
+            ->setParameter('code', $code)
+            ->getQuery()
+            ->getOneOrNullResult()
         ;
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Entity\Brand\Brand;
 use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductRepository as BaseProductRepository;
 
 final class ProductRepository extends BaseProductRepository
 {
-    public function findBranded(): array
+    public function findBranded(Brand $brand): array
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.brand IS NOT NULL')
+            ->andWhere('p.brand = :brand')
+            ->setParameter('brand', $brand)
             ->getQuery()
             ->getResult()
         ;
